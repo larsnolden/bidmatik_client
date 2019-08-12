19eaa7c0e6df3151ef9e855a59e14c42c367881d
Lars Nolden <lars@bidmatik.com>
2019-08-12 10:24:44 +0200

    add graphql and connect performance metrics component to query

--- src/components/performancePanel/MetricSelector.jsx
+++ src/components/performancePanel/MetricSelector.jsx
@@ -55,7 +55,7 @@ const MetricSelector = ({
   revenue,
   clicks,
   spend,
-  blendedAcos,
+  totalAcos,
   totalRevenue,
   impressions,
   selectedMetrics,
@@ -85,7 +85,7 @@ const MetricSelector = ({
       <Item onClick={() => handleSelectMetric(ACOS)} active={isActive(ACOS)}>
         acos
         <Metric active={isActive(ACOS)} primary={isPrimary(ACOS)}>
-          {acos * 100}
+          {Number(acos * 100).toFixed(2)}
           %
         </Metric>
       </Item>
@@ -112,7 +112,7 @@ const MetricSelector = ({
       <Item onClick={() => handleSelectMetric(BLENDEDACOS)} active={isActive(BLENDEDACOS)}>
         blended acos
         <Metric active={isActive(BLENDEDACOS)} primary={isPrimary(BLENDEDACOS)}>
-          {blendedAcos * 100}
+          {Number(totalAcos * 100).toFixed(2)}
           %
         </Metric>
       </Item>
@@ -133,15 +133,16 @@ const MetricSelector = ({
   );
 };
 
-//  TODO: remove test values
 MetricSelector.defaultProps = {
-  acos: 0.4,
-  revenue: 12142,
-  clicks: 141241,
-  spend: 412,
-  blendedAcos: 0.12,
-  totalRevenue: 22142,
-  impressions: 541142,
+  acos: 0,
+  revenue: 0,
+  clicks: 0,
+  spend: 0,
+  totalAcos: 0,
+  totalRevenue: 0,
+  impressions: 0,
+  selectedMetrics: () => {},
+  handleMetricsChange: () => {},
 };
 
 MetricSelector.propTypes = {
@@ -149,9 +150,11 @@ MetricSelector.propTypes = {
   revenue: propTypes.number,
   clicks: propTypes.number,
   spend: propTypes.number,
-  blendedAcos: propTypes.number,
+  totalAcos: propTypes.number,
   totalRevenue: propTypes.number,
   impressions: propTypes.number,
+  selectedMetrics: propTypes.func,
+  handleMetricsChange: propTypes.func,
 };
 
 export default MetricSelector;
--- src/components/performancePanel/PerformancePanel.jsx
+++ src/components/performancePanel/PerformancePanel.jsx
@@ -1,43 +1,85 @@
-import React, { useState } from 'react';
-import styled from '@emotion/styled';
+import {
+  compose,
+  withProps,
+  branch,
+  renderComponent,
+} from 'recompose';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import * as R from 'ramda';
 import moment from 'moment';
 
-import DateSelection from './DateSelection';
-import MetricSelector from './MetricSelector';
-import {
-  ACOS,
-  REVENUE,
-} from './constants';
+import PerformancePanelComponent from './PerformancePanelComponent';
 
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
+const GET_CAMPAIGNS_AND_PERFORMANCE = gql`
+  query performance_and_campaigns($from: Date, $to: Date){
+    campaigns(from: $from, to: $to){
+      id
+      name
+      type
+      targeting
+      budget
+      impressions
+      clicks
+      ctr
+      spend
+      cpc
+      orders
+      revenue
+      acos
+      portfolio
+    }
+    accountPerformance {
+      totalRevenue
+      totalAcos
+    }
+  }
 `;
 
-const PeformancePanel = () => {
-  const [selectedDates, setSelectedDates] = useState({
-    from: moment(moment.now()).subtract(60, 'days'),
-    to: moment(moment.now()),
-  });
+const addProps = withProps(({ data  }) => {
+  const { campaigns, accountPerformance } = data;
+  const reduced = {
+    acos: R.pipe(R.map(R.prop('acos')), R.mean)(campaigns),
+    budget: R.pipe(R.map(R.prop('budget')), R.sum)(campaigns),
+    clicks: R.pipe(R.map(R.prop('clicks')), R.sum)(campaigns),
+    cpc: R.pipe(R.map(R.prop('cpc')), R.mean)(campaigns),
+    ctr: R.pipe(R.map(R.prop('ctr')), R.mean)(campaigns),
+    impressions: R.pipe(R.map(R.prop('impressions')), R.sum)(campaigns),
+    orders: R.pipe(R.map(R.prop('orders')), R.sum)(campaigns),
+    revenue: R.pipe(R.map(R.prop('revenue')), R.sum)(campaigns),
+    spend: R.pipe(R.map(R.prop('spend')), R.sum)(campaigns),
+    totalAcos: accountPerformance.totalAcos,
+    totalRevenue: accountPerformance.totalRevenue,
+  };
+  return {
+    metrics: reduced,
+    loading: false,
+    dateFrom: data.variables.from,
+    dateTo: data.variables.to,
+    handleDateRangeChange: ({ dateFrom, dateTo }) => data.refetch({ from: dateFrom, to: dateTo }),
+  };
+});
 
-  const [selectedMetrics, setSelectedMetrics] = useState({
-    primary: ACOS,
-    secondary: REVENUE,
-  });
+const waitWhileLoading = (component, propName = 'data') => branch(
+  props => props[propName] && props[propName].loading,
+  renderComponent(component),
+);
 
-  return (
-    <Container>
-      <DateSelection
-        dateRange={selectedDates}
-        handleDateRangeChange={setSelectedDates}
-      />
-      <MetricSelector
-        selectedMetrics={selectedMetrics}
-        handleMetricsChange={setSelectedMetrics}
-      />
-    </Container>
-  );
-};
+const setLoadingFalse = withProps(() => ({
+  loading: false,
+}));
 
-export default PeformancePanel;
+export default compose(
+  graphql(GET_CAMPAIGNS_AND_PERFORMANCE, {
+    options: {
+      variables: {
+        from: moment(moment.now()).subtract(60, 'days'),
+        to: moment(moment.now()),
+      },
+    },
+  }),
+  waitWhileLoading(PerformancePanelComponent),
+  addProps,
+  setLoadingFalse,
+)(PerformancePanelComponent);
