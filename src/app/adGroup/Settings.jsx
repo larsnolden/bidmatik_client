63dcaca34b4708ba9221b5036a06f2d50b0f09c1
lars <larsnolden@web.de>
2019-10-03 10:41:24 +0200

    add settings pane modal

--- src/app/adGroup/Settings.jsx
+++ src/app/adGroup/Settings.jsx
@@ -7,19 +7,21 @@ const SettingsButton = styled.div`
   line-height: 20px;
   letter-spacing: 0.025em;
   text-transform: capitalize;
+  color: #186faf;
 `;
-const GearIcon = styled.div``;
+const GearIcon = styled.img``;
 const Container = styled.div``;
 
 const Settings = () => {
-  // const [isModalOpen, setIsModalOpen] = useState(false);
-  // if (!isModalOpen)
-  return (
-    <SettingsButton>
-      Settings
-      <GearIcon src={GearIconPath} />
-    </SettingsButton>
-  );
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  if (!isModalOpen)
+    return (
+      <SettingsButton>
+        Settings
+        <GearIcon src={GearIconPath} />
+      </SettingsButton>
+    );
+  return <Container>some</Container>;
 };
 
 export default Settings;
